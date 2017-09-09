d2d7213b1111801cf3a5a73729c963ac566220d5
raghava.u <raghava.u@gmail.com>
2017-09-09 10:59:01 +0530

    new files added, old files delete and script changes

--- www/angular/view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('View', [])
-.controller('LiveTrackingController', function ($scope) {
-    $scope.title = 'Live Tracking';
-})
-.controller('DataController', function ($scope) {
-    $scope.title = 'Data';
-});
